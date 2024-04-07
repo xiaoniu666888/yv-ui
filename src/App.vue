36c0c8b191185c611523731718f60baa9eb0e545
xiaoniu <270146073@qq.com>
2024-04-07 12:20:10 +0800

    button(1)

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,54 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Button from './components/Button/Button.vue'
+import { ref, onMounted } from 'vue';
+import type { ButtonInstance } from './components/Button/types';
+
+const buttonRef = ref<ButtonInstance | null>(null)
+onMounted(() => {
+  if (buttonRef.value)
+    console.log(buttonRef.value.ref)
+})
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <Button type="danger" ref="buttonRef" plain disabled> Test Button </Button>
+    </div>
+  </header>
+
+  <main>
+
+  </main>
 </template>
 
 <style scoped>
+header {
+  line-height: 1.5;
+}
+
 .logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+  display: block;
+  margin: 0 auto 2rem;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
 }
 </style>
---
+++ src/components/Button/Button.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import type { ButtonProps } from './types';
+defineOptions({
+    name: "YButton"
+})
+withDefaults(defineProps<ButtonProps>(), {
+    nativeType: 'button'
+})
+const _ref = ref<HTMLButtonElement>()
+defineExpose({
+    ref: _ref
+})
+</script>
+
+<template>
+    <div>
+        <button ref="_ref" class="yv-button" :class="{
+            [`yv-button--${type}`]: type,
+            [`yv-button--${size}`]: size,
+            'is-plain': plain,
+            'is-round': round,
+            'is-circle': circle,
+            'is-disabled': disabled
+        }" :disabled="disabled" :autofocus="autofocus" :type="nativeType">
+            <span>
+                <slot></slot>
+            </span>
+        </button>
+    </div>
+</template>
+
+
+<style scoped></style>
