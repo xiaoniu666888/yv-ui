506134801bceac9a641e7993de24f465cc62827e
xiaoniu <270146073@qq.com>
2024-04-14 11:37:14 +0800

    select:准备

---
+++ src/components/Select/Select.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import Tooltip from '../Tooltip/Tooltip.vue';
+import Input from '../Input/Input.vue';
+
+
+import type { SelectsProps, SelectEmits, SelectOption } from './types'
+
+
+const props = defineProps<SelectsProps>()
+const emits = defineEmits<SelectEmits>()
+const innerValue = ref('')
+</script>
+
+
+<template>
+    <div class="yv-select" :class="{
+        'is-disabled': disabled
+
+    }">
+        <Tooltip placement="bottom-start" manual>
+            <Input v-model="innerValue" :disabled="disabled" :placeholder="placeholder" />
+            <template #content>
+                <ul class="yv-select__menu">
+                    <template v-for="(item, index) in options" :key="index">
+                        <li class="yv-select__menu-item" :class="{ 'is-disabled': item.disabled }"
+                            :id="`select-item-${item.value}`">
+                            {{ item.label }}
+                        </li>
+                    </template>
+                </ul>
+            </template>
+        </Tooltip>
+    </div>
+</template>
+
+
+<style scoped></style>
---
+++ src/components/Select/types.ts
@@ -0,0 +1,20 @@
+export interface SelectOption {
+  label: string
+  value: string
+  disabled?: boolean
+}
+
+export interface SelectsProps {
+  // v-model
+  modelValue: string
+  // 选项
+  options: SelectOption[]
+  //   表单属性
+  placeholder: string
+  disabled: boolean
+}
+export interface SelectEmits {
+  (e: 'change', value: string): void
+  (e: 'update:modelValue', value: string): void
+  (e: 'visible-change', value: boolean): void
+}
