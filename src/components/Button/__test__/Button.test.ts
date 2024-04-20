a9206c723d3605b12712978267cbcec7ae1cc080
xiaoniu <270146073@qq.com>
2024-04-20 17:49:22 +0800

    add test

--- src/components/Button/__test__/Button.test.ts
+++ src/components/Button/__test__/Button.test.ts
@@ -5,64 +5,72 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import Icon from '../../Icon/Icon.vue'
 describe('Button.vue', () => {
   // primary
-  test('primary button', () => {
-    const wrapper = mount(Button, {
-      props: {
-        type: 'primary'
-      },
-      slots: {
-        default: 'button'
-      }
-    })
-    console.log(wrapper.html())
-    expect(wrapper.classes()).toContain('yv-button--primary')
-    // slot
-    expect(wrapper.get('button').text()).toBe('button')
-
-    // events
-    wrapper.get('button').trigger('click')
-    console.log('emitted=>', wrapper.emitted())
-    expect(wrapper.emitted()).toHaveProperty('click')
-  })
-
+  // test('primary button', () => {
+  //   const wrapper = mount(Button, {
+  //     props: {
+  //       type: 'primary'
+  //     },
+  //     slots: {
+  //       default: 'primary button'
+  //     }
+  //   })
+  //   console.log(wrapper.html())
+  //   expect(wrapper.classes()).toContain('yv-button--primary')
+  //   // slot
+  //   expect(wrapper.get('button').text()).toBe('primary button')
+  //   // events
+  //   wrapper.get('button').trigger('click')
+  //   console.log('emitted=>', wrapper.emitted())
+  //   expect(wrapper.emitted()).toHaveProperty('click')
+  // })
   // disabled
-  test('disabled button', () => {
-    const wrapper = mount(Button, {
-      props: {
-        disabled: true
-      },
-      slots: {
-        default: 'default_button'
-      }
-    })
-    // 获取属性
-    expect(wrapper.attributes('disabled')).toBeDefined()
-    // 获取原生DOM节点再获取属性
-    expect(wrapper.find('button').element.disabled)
-    expect(wrapper.get('button').text()).toBe('default_button')
-    wrapper.get('button').trigger('click')
-    expect(wrapper.emitted()).not.toHaveProperty('cilck')
-  })
-
-  // icon
-  test('icon button', () => {
-    const wrapper = mount(Button, {
-      props: {
-        icon: 'arrow-up'
-      },
-      slots: {
-        default: 'icon'
-      },
-      global: {
-        stubs: ['FontAwesomeIcon']
-      }
-    })
-    const iconElement = wrapper.findComponent(FontAwesomeIcon)
-    expect(iconElement.exists()).toBeTruthy()
-    expect(iconElement.attributes('icon')).toBe('arrow-up')
-  })
-
-  // loading
+  // test('disabled button', () => {
+  //   const wrapper = mount(Button, {
+  //     props: {
+  //       disabled: true
+  //     },
+  //     slots: {
+  //       default: 'default_button'
+  //     }
+  //   })
+  //   console.log(wrapper.html())
+  //   // 测试时候是否具有 class 类名
+  //   expect(wrapper.classes()).toContain('is-disabled')
+  //   //  测试属性disabled被添加
+  //   expect(wrapper.attributes('disabled')).toBeDefined()
+  //   // 获取原生DOM节点之后再获取属性
+  //   expect(wrapper.find('button').element.disabled).toBe(true)
+  //   // 获取按钮中的文字
+  //   expect(wrapper.get('button').text()).toBe('default_button')
+  //   // 触发点击事件
+  //   wrapper.get('button').trigger('click')
+  //   expect(wrapper.emitted()).not.toHaveProperty('cilck')
+  // })
+  // // icon
+  // test('icon button', () => {
+  //   const wrapper = mount(Button, {
+  //     props: {
+  //       icon: 'arrow-up'
+  //     },
+  //     slots: {
+  //       default: 'icon'
+  //     },
+  //     global: {
+  //       // stubs   stubs 是用来替换组件测试中复杂或难以模拟的子组件的
+  //       // FontAwesomeIcon 组件被指定为一个 stub。这意味着在测试中，任何 FontAwesomeIcon 组件的实例都会被替换为一个简单的、不包含实际逻辑或渲染的占位符组件。这有助于减少测试的复杂性，因为你不需要关注 FontAwesomeIcon 组件的具体实现细节，而只需要确保它正确地被渲染到了父组件中。
+  //       stubs: ['FontAwesomeIcon']
+  //     }
+  //   })
+  //   console.log(wrapper.html())
+  //   // 找到  FontAwesomeIcon  组件
+  //   const iconElement = wrapper.findComponent(FontAwesomeIcon)
+  //   // 组件存在
+  //   expect(iconElement.exists()).toBeTruthy()
+  //   // 组件具有icon属性且为 arrow-up
+  //   // expect(iconElement.attributes('icon')).toBeDefined()
+  //   expect(iconElement.attributes('icon')).toBe('arrow-up')
+  // })
+  // // loading
   test('loading button', () => {
     const wrapper = mount(Button, {
       props: {
@@ -80,5 +88,7 @@ describe('Button.vue', () => {
     expect(iconElement.exists()).toBeTruthy()
     expect(iconElement.attributes('icon')).toBe('spinner')
     expect(wrapper.attributes('disabled')).toBeDefined()
+    expect(wrapper.find('button').element.disabled).toBe(true)
+    expect(wrapper.attributes('disabled'))
   })
 })
--- src/components/Collapse/__test__/Collapse.test.tsx
+++ src/components/Collapse/__test__/Collapse.test.tsx
@@ -1,19 +1,35 @@
 import Collapse from '../Collapse.vue'
-// import CollapseItem from '../CollapseItem.vue'
+import CollapseItem from '../CollapseItem.vue'
 
-import { test, describe } from 'vitest'
+import { test, describe, expect } from 'vitest'
 import { mount } from '@vue/test-utils'
 
 describe('Collaspe.vue', () => {
   test('basic collapse', () => {
-    const colEl = mount(Collapse, {
-      psops: {
-        modelValue: ['a']
+    const colElWrapper = mount(Collapse, {
+      props: {
+        'modelValue': ['a']
+      },
+      slots: {
+        default: 
+        (<>
+          <CollapseItem name="a" title="title a">
+            content a
+          </CollapseItem>
+          <CollapseItem name="b" title="title b">
+            content b
+          </CollapseItem>
+          <CollapseItem name="c" title="title c" disabled>
+            content c
+          </CollapseItem>
+        </>)
       },
       global: {
-        stubs: ['CollapseItem']
+        stubs: ['Icon']
       }
     })
-    console.log(colEl.html())
+    console.log(colElWrapper.html())
+    const CollapseItemEl  =  colElWrapper.findComponent(CollapseItem)
+    expect(CollapseItemEl.exists()).toBeTruthy()
   })
 })
